575d76cd74f1bfb75517cf161d60fd7ebf4cf0a6
SharkyMf <sharkymf@gmail.com>
2018-10-30 15:00:14 +0200

    fix

--- src/components/ProductService.js
+++ src/components/ProductService.js
@@ -3,44 +3,47 @@ import _ from 'lodash';
 
 
 export default new Vue({
-  data: {
-    products: [
-      {
-        id: 1,
-        title: 'Product #1',
-        price: 50,
-        qt: 1000,
-      },
-      {
-        id: 2,
-        title: 'Product #2',
-        price: 100,
-        qt: 456,
-      },
-      {
-        id: 3,
-        title: 'Product #3',
-        price: 23,
-        qt: 23445,
-      },
-      {
-        id: 4,
-        title: 'Product #4',
-        price: 50,
-        qt: 1000,
-      },
-    ],
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          title: 'Product #1',
+          price: 50,
+          qt: 1000,
+        },
+        {
+          id: 2,
+          title: 'Product #2',
+          price: 100,
+          qt: 456,
+        },
+        {
+          id: 3,
+          title: 'Product #3',
+          price: 23,
+          qt: 23445,
+        },
+        {
+          id: 4,
+          title: 'Product #4',
+          price: 50,
+          qt: 1000,
+        },
+      ],
+    };
   },
   methods: {
     viewDetails(id) {
-      const productToView = _.find(this.products, { id });
+      const productToView = _.find(this.products, {id});
       this.$emit('viewDetails1', productToView);
       console.log('1', productToView);
     },
     addProduct(title, price, qt) {
-      const id = this.product[this.product.length - 1] + 1;
+      console.log('1111111111 ', this.products);
+      const id = this.products[this.products.length - 1] + 1;
       console.log('1', id);
-      this.product.push({
+      this.products.push({
         id,
         title,
         price,
